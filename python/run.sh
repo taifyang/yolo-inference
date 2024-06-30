9350c760a57b23a3b4840539078a065a3744d7af
taifyang <58515915+taifyang@users.noreply.github.com>
2024-06-30 10:35:29 +0800

    update readme and some bug fix
    update readme and some bug fix

--- python/run.sh
+++ python/run.sh
@@ -1,40 +1,40 @@
-# python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
-# python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
-# python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx 
-# python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx
-# python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type INT8 --model_path yolov5n_cls_int8.onnx
-# python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type INT8 --model_path yolov5n_cls_int8.onnx
-# python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
-# python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
-# python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx
-# python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx
-# python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP32 --model_path yolov5n_cls_fp32.xml
-# python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.xml
-# python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP16 --model_path yolov5n_cls_fp16.xml
-# python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.xml
-# python main.py --algo_type YOLOv5 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.engine
-# python main.py --algo_type YOLOv5 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.engine
-# python main.py --algo_type YOLOv5 --backend_type TensorRT --task_type Classify --device_type GPU --model_type INT8 --model_path yolov5n_cls_int8.engine
+python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
+python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
+python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx 
+python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx
+python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type INT8 --model_path yolov5n_cls_int8.onnx
+python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type INT8 --model_path yolov5n_cls_int8.onnx
+python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
+python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.onnx
+python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx
+python main.py --algo_type YOLOv5 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.onnx
+python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP32 --model_path yolov5n_cls_fp32.xml
+python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.xml
+python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP16 --model_path yolov5n_cls_fp16.xml
+python main.py --algo_type YOLOv5 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.xml
+python main.py --algo_type YOLOv5 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP32 --model_path yolov5n_cls_fp32.engine
+python main.py --algo_type YOLOv5 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP16 --model_path yolov5n_cls_fp16.engine
+python main.py --algo_type YOLOv5 --backend_type TensorRT --task_type Classify --device_type GPU --model_type INT8 --model_path yolov5n_cls_int8.engine
 
-# python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
-# python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
-# python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx 
-# python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx
-# python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type INT8 --model_path yolov8n_cls_int8.onnx
-# python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type INT8 --model_path yolov8n_cls_int8.onnx
-# python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
-# python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
-# python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx
-# python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx
+python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
+python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
+python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx 
+python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx
+python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type CPU --model_type INT8 --model_path yolov8n_cls_int8.onnx
+python main.py --algo_type YOLOv8 --backend_type ONNXRuntime --task_type Classify --device_type GPU --model_type INT8 --model_path yolov8n_cls_int8.onnx
+python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
+python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.onnx
+python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type CPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx
+python main.py --algo_type YOLOv8 --backend_type OpenCV --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.onnx
 
-# python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP32 --model_path yolov8n_cls_fp32.xml
-# python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.xml
-# python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP16 --model_path yolov8n_cls_fp16.xml
-# python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.xml
+python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP32 --model_path yolov8n_cls_fp32.xml
+python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.xml
+python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type CPU --model_type FP16 --model_path yolov8n_cls_fp16.xml
+python main.py --algo_type YOLOv8 --backend_type OpenVINO --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.xml
 
-# python main.py --algo_type YOLOv8 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.engine
-# python main.py --algo_type YOLOv8 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.engine
-# python main.py --algo_type YOLOv8 --backend_type TensorRT --task_type Classify --device_type GPU --model_type INT8 --model_path yolov8n_cls_int8.engine
+python main.py --algo_type YOLOv8 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP32 --model_path yolov8n_cls_fp32.engine
+python main.py --algo_type YOLOv8 --backend_type TensorRT --task_type Classify --device_type GPU --model_type FP16 --model_path yolov8n_cls_fp16.engine
+python main.py --algo_type YOLOv8 --backend_type TensorRT --task_type Classify --device_type GPU --model_type INT8 --model_path yolov8n_cls_int8.engine
 
 
 python main.py --algo_type YOLOv5 --backend_type ONNXRuntime --task_type Detect --device_type CPU --model_type FP32 --model_path yolov5n_fp32.onnx
