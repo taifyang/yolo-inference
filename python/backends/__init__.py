83736f293d3d4d94f933b16cc17aa955d4990abe
taifyang <58515915+taifyang@users.noreply.github.com>
2024-11-08 21:34:20 +0800

    update readme

--- python/backends/__init__.py
+++ python/backends/__init__.py
@@ -2,37 +2,32 @@
 Author: taifyang 
 Date: 2024-07-11 23:48:57
 LastEditors: taifyang 
-LastEditTime: 2024-10-31 23:02:56
+LastEditTime: 2024-11-07 21:14:54
 FilePath: \yolo-inference\python\backends\__init__.py
 '''
 
 
 try:
-    import onnxruntime
     from backends import ONNXRuntime
 except:
     print('ONNXRuntime module import fail!')
     
 try:
-    import cv2
     from backends import OpenCV
 except:
     print('OpenCV module import fail!')
    
 try:
-    import openvino
     from backends import OpenVINO
 except:
     print('OpenVINO module import fail!')
 
 try:
-    import torch
     from backends import PyTorch
 except:
     print('PyTorch module import fail!')
             
 try:
-    import tensorrt
     from backends import TensorRT
 except:
     print('TensorRT module import fail!')
